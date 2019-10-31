50302addc51f088445f396848a5d4860ee965e2c
Mitch Eagles <mitch.eagles.stl@gmail.com>
2019-10-30 23:06:38 -0500

    Basic display pre-parsed data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import _ from 'underscore';
+
+import PublicSafetyData_2018 from './output/2018-public-safety-logs.json'
+// console.log(PublicSafetyData_2018);
+
+const DataRows = _.map(PublicSafetyData_2018, (Value, Key) => {
+  return <div key={Key}>
+    <h2>{Key}</h2>
+    <div>
+      {_.map(Value, (NumCalls, CallType) => {
+        return <div key = {Key+CallType}>
+          <span style={{weight:'bold'}}>{CallType}:</span>
+          <span>{NumCalls}</span>
+        </div>
+      })}
+    </div>
+  </div>
+});
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {DataRows}
       </header>
     </div>
   );
